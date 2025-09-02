740c75a1e23330b925eeb54ce46b0eaf531e18ab
kg1fpclarsim <kg1fpclarsim@hotmail.com>
2025-09-02 16:10:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,42 +8,64 @@ document.addEventListener('DOMContentLoaded', () => {
     const ORIGINAL_IMAGE_WIDTH = 426; 
     
     // Dina koordinater är nu infogade här nedanför.
-    const topLevelMenu = {
-        image: 'handdator.png',
-        events: [
-            { name: "Lasta ut", coords: { top:  197, left: 71, width: 138, height: 76 } },
-            { name: "Lossa in", coords: { top: 197, left: 221, width: 138, height: 76 } },
-            { 
-                name: "Hämta", 
-                coords: { top: 287, left: 71, width: 138, height: 76 },
-                submenu: {
-                    image: 'handdator-hamta.png',
-                    backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
-                    events: [
-                        { name: "Hämta åt annan bil", coords: { top: 295, left: 70, width: 185, height: 30 } },
-                        { name: "Hämta obokad hämtning", coords: { top: 240, left: 70, width: 185, height: 30 } }
-                    ]
-                }
-            },
-            { name: "Leverera", coords: { top: 287, left: 221, width: 138, height: 76 } },
-            { name: "Bomhämtning", coords: { top: 374, left: 71, width: 138, height: 76 } },
-            { name: "Ej levererat", coords: { top: 374, left: 221, width: 138, height: 76 } },
-            { name: "Hämtning utan sändnings-ID", coords: { top: 463, left: 71, width: 138, height: 76 } },
-            { name: "Åter terminal", coords: { top: 463, left: 221, width: 138, height: 76 } },
-            { 
-                name: "Flänsa", 
-                coords: { top: 552, left: 71, width: 138, height: 76 },
-                submenu: {
-                    image: 'handdator-flansa.png',
-                    backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
-                    events: [
-                        { name: "Flänsa på", coords: { top: 197, left: 71, width: 138, height: 76 } },
-                        { name: "Flänsa av", coords: { top: 197, left: 221, width: 138, height: 76 } }
-                    ]
-                }
+  const topLevelMenu = {
+    image: 'handdator.png',
+    events: [
+        { name: "Lasta ut", coords: { top:  197, left: 71, width: 138, height: 76 } },
+        { name: "Lossa in", coords: { top: 197, left: 221, width: 138, height: 76 } },
+        { 
+            name: "Hämta", 
+            coords: { top: 287, left: 71, width: 138, height: 76 },
+            submenu: {
+                image: 'handdator-hamta.png',
+                backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
+                events: [
+                    { name: "Hämta åt annan bil", coords: { top: 295, left: 70, width: 185, height: 30 } },
+                    { name: "Hämta obokad hämtning", coords: { top: 240, left: 70, width: 185, height: 30 } }
+                ]
             }
-        ]
-    };
+        },
+        { name: "Leverera", coords: { top: 287, left: 221, width: 138, height: 76 } },
+        { name: "Bomhämtning", coords: { top: 374, left: 71, width: 138, height: 76 } },
+        { name: "Ej levererat", coords: { top: 374, left: 221, width: 138, height: 76 } },
+        { name: "Hämtning utan sändnings-ID", coords: { top: 463, left: 71, width: 138, height: 76 } },
+        { name: "Åter terminal", coords: { top: 463, left: 221, width: 138, height: 76 } },
+        { 
+            name: "Flänsa", 
+            coords: { top: 552, left: 71, width: 138, height: 76 },
+            submenu: {
+                image: 'handdator-flansa.png',
+                backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
+                events: [
+                    { name: "Flänsa på", coords: { top: 197, left: 71, width: 138, height: 76 } },
+                    { name: "Flänsa av", coords: { top: 197, left: 221, width: 138, height: 76 } }
+                ]
+            }
+        },
+        { 
+            name: "Hem", 
+            coords: { top: 655, left: 90, width: 35, height: 50 },
+            submenu: {
+                image: 'handdator-hem.png',
+                backButtonCoords: { top: 655, left: 175, width: 80, height: 50 },
+                events: [
+                    { 
+                        name: "Synkronisera visa",
+                        coords: { top: 385, left: 70, width: 80, height: 25 },
+                        // KORRIGERAD KOD BÖRJAR HÄR
+                        submenu: { // Rättad från "submenu;"
+                            image: 'handdator-senastehandelse.png',
+                            backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
+                            events: [
+                                { name: "Ångra", coords: { top: 240, left: 300, width: 60, height: 30 } }
+                            ]
+                        } 
+                    }
+                ]
+            }
+        }
+    ]
+};
     // ####################################################################
     // ### SLUT PÅ SEKTIONEN MED DINA VÄRDEN                            ###
     // ####################################################################
