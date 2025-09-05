6c3189f8fa52ed05ff7d015ffe68c600a7ea9f62
kg1fpclarsim <kg1fpclarsim@hotmail.com>
2025-09-05 14:04:53 +0200

    Create event-definitions.js

---
+++ analys/event-definitions.js
@@ -0,0 +1,10 @@
+// event-definitions.js
+const ANALYS_EVENTS = [
+    { name: "Hämta" },
+    { name: "Lossa in" },
+    { name: "Lasta ut" },
+    { name: "Leverera" },
+    { name: "Åter terminal" },
+    { name: "Ej levererat" },
+    { name: "Bekräfta Orsak" }
+];
