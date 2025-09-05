18389fb7888a63114a6921d7f1e60ca9cc08d84e
kg1fpclarsim <kg1fpclarsim@hotmail.com>
2025-09-05 15:32:17 +0200

    Update event-definitions.js

--- analys/event-definitions.js
+++ analys/event-definitions.js
@@ -1,10 +1,45 @@
 // event-definitions.js
+
 const ANALYS_EVENTS = [
-    { name: "Hämta" },
-    { name: "Lossa in" },
-    { name: "Lasta ut" },
-    { name: "Leverera" },
-    { name: "Åter terminal" },
-    { name: "Ej levererat" },
-    { name: "Bekräfta Orsak" }
+    { 
+        name: "Lasta ut",
+        image: "images/analysis_buttons/lasta-ut.png"
+    },
+    { 
+        name: "Lossa in",
+        image: "images/analysis_buttons/lossa-in.png"
+    },
+    { 
+        name: "Leverera",
+        image: "images/analysis_buttons/leverera.png"
+    },
+    { 
+        name: "Bomhämtning",
+        image: "images/analysis_buttons/bomhamtning.png"
+    },
+    { 
+        name: "Hämta åt annan bil",
+        // Du behöver skapa en bild för denna knapp
+        image: "images/analysis_buttons/hamta-annan-bil.png" 
+    },
+    { 
+        name: "Hämta obokad hämtning",
+        // Du behöver skapa en bild för denna knapp
+        image: "images/analysis_buttons/hamta-obokad.png"
+    },
+    { 
+        name: "Flänsa på",
+        // Du behöver skapa en bild för denna knapp
+        image: "images/analysis_buttons/flansa-pa.png"
+    },
+    { 
+        name: "Flänsa av",
+        // Du behöver skapa en bild för denna knapp
+        image: "images/analysis_buttons/flansa-av.png"
+    },
+    { 
+        name: "Åter terminal",
+        image: "images/analysis_buttons/ater-terminal.png"
+    }
+    // Notera: Navigationsknappar som "Hämta", "Flänsa", "Hem" etc. är medvetet exkluderade.
 ];
