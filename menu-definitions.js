311bf1e17e2bcd3a0ac5629ffd51a184b67dd483
kg1fpclarsim <kg1fpclarsim@hotmail.com>
2025-09-30 13:16:59 +0200

    Update menu-definitions.js

--- menu-definitions.js
+++ menu-definitions.js
@@ -33,7 +33,7 @@ const ALL_MENUS = {
         backButtonCoords: { top: 145, left: 70, width: 20, height: 25 },
         textOverlays: [
             { id: 'selected-office-display', coords: { top: 185, left: 140, width: 170, height: 25 } },
-            { id: 'selected-office-display', coords: { top: 653, left: 222, width: 120, height: 30 } }
+            { id: 'selected-office-display', coords: { top: 653, left: 222, width: 120, height: 30 }, mirrorSelectors: ['#selected-office-readout']  }
         ],
         events: [
             { name: "Kontor", type: "dropdown",
